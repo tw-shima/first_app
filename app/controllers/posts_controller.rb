1be66da6acfce993a06b939e5050444da9903966
tw-shima <168060800+tw-shima@users.noreply.github.com>
2024-04-25 15:28:06 +0900

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,14 @@
+class PostsController < ApplicationController
+
+  def index # indexアクションを定義した
+    @posts = Post.all #　すべてのレコードを@postに代入
+  end
+  
+  def new
+  end
+
+  def create
+    Post .create(content: params[:content])
+    redirect_to "/postss"
+  end 
+end
